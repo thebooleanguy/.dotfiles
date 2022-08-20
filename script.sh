3035f70bad7a2a8ccc8a5839c5f32b608e72e585
thebooleanguy <tharindus0601@gmail.com>
2022-08-20 10:13:20 +0530

    added some more stow pkgs and removed some. Added Keybinding and Installed-PKGS list

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 # restore listed .dotfile packages
-stow caffeine dconf fish hyprland kitty mpv rhythmbox spacevim starship 
+stow caffeine dconf fish hyprland kitty mpv neovim rhythmbox starship 
 
 # restore all gnome settings saved in dconf
 dconf load / < ~/.config/dconf/user.conf
